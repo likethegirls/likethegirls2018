40ec5870a0be8cfe30a5a9f7228e403d9491fc03
likethegirls <likethegirls8@gmail.com>
2018-06-05 06:14:30 +0530

    Category format

--- template-parts/content.php
+++ template-parts/content.php
@@ -37,25 +37,19 @@
 		<header class="entry-header">
 			<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
 				<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
-			<?php endif; ?>
-
-			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); 
-			?>
-
-		</header><!-- .entry-header -->
-		<footer class="entry-footer">
-			<?php twentysixteen_entry_meta(); ?>
-			<?php
-				edit_post_link(
-					sprintf(
-						/* translators: %s: Name of current post */
-						__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-						get_the_title()
-					),
+			<?php endif; 
+			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); 
+				twentysixteen_entry_meta(); 
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+					get_the_title()
+				),
 					'<span class="edit-link">',
 					'</span>'
 				);
 			?>
-		</footer><!-- .entry-footer -->
+		</header><!-- .entry-header -->
 	</div>
 </article><!-- #post-## -->

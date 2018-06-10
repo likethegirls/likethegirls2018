da760dcd31c3aa5e5524d8f2c0ebe96c439a51ec
Tristan Jensen <tristanjensen@Tristans-MacBook-Pro-2.local>
2018-06-10 18:30:01 +1000

    polishing home feed

--- template-parts/content.php
+++ template-parts/content.php
@@ -33,11 +33,11 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-	<div class="entry-titles col-xs-12">
+		<div class="entry-titles col-xs-10 col-xs-push-1 mt-xs-1">
 		<header class="entry-header">
 			<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
 				<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
-			<?php endif; 
+			<?php endif;
 			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			echo('<p class="mt-xs-1">'); the_excerpt(); ?>
 			</p>

70b15d9844a6b2e35dfcea140f4ae638f25a0f08
likethegirls <likethegirls8@gmail.com>
2018-06-27 07:37:34 +0200

    changing spacing for paginated items

--- single.php
+++ single.php
@@ -32,13 +32,13 @@ get_header(); ?>
 				 // Previous/next post navigation.
 			    $next_post = get_next_post();
 			    $previous_post = get_previous_post();
-			    the_post_navigation( array(
-		        'next_text' => '<span class="meta-nav col-xs-12 col-sm-6" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-		            '<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-		            '<span class="post-title">%title</span>' . get_the_post_thumbnail($next_post->ID,'thumbnail'),
-		        'prev_text' => '<span class="meta-nav col-xs-12 col-sm-6" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
+			    the_post_navigation( array('div class="col-xs-6">' .
+		        'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
+		            '<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span>' .
+		            '<span class="post-title">%title</span>' . get_the_post_thumbnail($next_post->ID,'thumbnail'), '</div><div class="col-xs-6">' .
+		        'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
 		            '<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-		            '<span class="post-title">%title</span>' . get_the_post_thumbnail($previous_post->ID,'thumbnail'),
+		            '<span class="post-title">%title</span>' . get_the_post_thumbnail($previous_post->ID,'thumbnail'), '</div>'
     ) );
 			}
 

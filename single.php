50c39c710b647b120fb513a71edfb7e50fcf4695
likethegirls <likethegirls8@gmail.com>
2018-06-27 07:32:07 +0200

    testing styling for meta nav

--- single.php
+++ single.php
@@ -29,23 +29,14 @@ get_header(); ?>
 					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
 				) );
 			} elseif ( is_singular( 'post' ) ) {
-				// Previous/next post navigation.
-				the_post_navigation( array(
-					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-				) );
 				 // Previous/next post navigation.
 			    $next_post = get_next_post();
 			    $previous_post = get_previous_post();
 			    the_post_navigation( array(
-		        'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
+		        'next_text' => '<span class="meta-nav col-xs-12 col-sm-6" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
 		            '<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
 		            '<span class="post-title">%title</span>' . get_the_post_thumbnail($next_post->ID,'thumbnail'),
-		        'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
+		        'prev_text' => '<span class="meta-nav col-xs-12 col-sm-6" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
 		            '<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
 		            '<span class="post-title">%title</span>' . get_the_post_thumbnail($previous_post->ID,'thumbnail'),
     ) );

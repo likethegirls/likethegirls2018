d947ed810252cc0e176fcdee0349f7f827ee87d3
likethegirls <likethegirls8@gmail.com>
2018-07-09 21:28:15 +1000

    Fixing desktop site

--- sidebar.php
+++ sidebar.php
@@ -9,7 +9,7 @@
 ?>
 
 <?php if ( is_active_sidebar( 'sidebar-1' )  ) : ?>
-	<aside id="secondary" class="sidebar widget-area col-sm-4 pull-right hidden-xs col-md-3 mt-xs-4 mt-sm-6" role="complementary">
+	<aside id="secondary" class="sidebar widget-area col-sm-4 pull-right hidden-xs col-md-4 mt-xs-4 mt-sm-6" role="complementary">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</aside><!-- .sidebar .widget-area -->
 <?php endif; ?>

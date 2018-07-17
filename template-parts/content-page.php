a87013e7d628dea8a6e2efb0bc7fd664f8b6a94d
likethegirls <likethegirls8@gmail.com>
2018-07-18 07:27:51 +1000

    Removed sidebar and styled about page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,12 +10,12 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title( '<h1 class="entry-title page-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<?php twentysixteen_post_thumbnail(); ?>
 
-	<div class="entry-content">
+	<div class="entry-content col-xs-12 col-sm-6 col-sm-push-3 center-block">
 		<?php
 		the_content();
 
